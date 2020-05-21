2d1368e18040669841c5fadca56f7980837ad661
Kyle Brainard <kbrainard@nevada.unr.edu>
2020-05-21 16:01:34 -0700

    Add current timer display

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { View, Linking } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, List, ListItem, Grid, Row, Tab, Tabs } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import moment from 'moment';
 
 interface PoolTimer
 {
@@ -13,6 +14,13 @@ interface PoolTimer
     start: string,
     summer_duration: string,
     winter_duration: string
+}
+
+interface CurrentTimer
+{
+    speed: number,
+    start: string,
+    end: string
 }
 
 const SERVER_NAME = 'http://192.168.4.32:5000';
@@ -51,9 +59,68 @@ const TimerList : React.FC = () => {
     } else {
         return (
             <List>
-                {items.map(item => <ListItem key={item.id.toString()}><Text>{item.speed} - {item.start}</Text></ListItem>)}
+                {items.map(item =>
+                    <ListItem key={item.id.toString()}>
+                        <Text>
+                            Speed: {item.speed} - {moment(item.start, 'HH:mm:ss').format('h:mma')}
+                        </Text>
+                    </ListItem>)}
             </List>
         );
+    }
+}
+
+const CurrentSpeed : React.FC = () => {
+    const [error, setError] = useState<{message:string} | null>(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [timer, setTimer] = useState<CurrentTimer | null>(null);
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch(SERVER_NAME + "/program/now")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setTimer(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    
+    if (error) {
+        return <Text>Error: {error.message}</Text>;
+    } else if (!isLoaded) {
+        return <AppLoading/>
+    } else if (timer != null) {
+        if(timer.speed == 0)
+        {
+            return (
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                    <Text>OFF</Text>
+                    <Text>Next Timer: {moment(timer.end, 'HH:mm:ss').format('h:mma')} </Text>
+                </View>
+            );
+        
+        } else {
+            return (
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                    <Text>Speed: {timer.speed}</Text>
+                    <Text>Started: {timer.start}, Next Timer: {timer.end}</Text>
+                </View>
+            );
+        }
+    } else {
+        return <Text>Error loading current timer (logic error)</Text>
     }
 }
 
@@ -83,22 +150,25 @@ const App : React.FC = () => {
                 </Header>
                 <Tabs>
                     <Tab heading="Dashboard">
-                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF'}}>
-                            <View style={{flex: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center', backgroundColor:'#F5FCFF'}}>
-                                <Text>Section 1</Text>
-                                <TimerList/>
+                        <View style={{flex: 1, margin: 10}}>
+                            <View style={{margin: 10, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center'}}>
+                                <CurrentSpeed/>
                             </View>
-                            <View style={{flex:1, alignSelf: 'stretch', backgroundColor:'#F5FCFF'}}>
-                                <Text>Section 2</Text>
-                                <TimerList/>
+                            <View style={{margin: 10, alignSelf: 'stretch'}}>
+                                <Text>Override</Text>
                             </View>
                         </View>
                     </Tab>
                     <Tab heading="Timers">
-                        <Text>Welcome to Timers!</Text>
+                        <View style={{flex: 1, margin: 10}}>
+                            <Text>Welcome to Timers!</Text>
+                            <TimerList/>
+                        </View>
                     </Tab>
                     <Tab heading="Settings">
-                    <Text>Welcome to Settings!</Text>
+                        <View style={{flex: 1, margin: 10}}>
+                            <Text>Welcome to Settings!</Text>
+                        </View>
                     </Tab>
                 </Tabs>
                 <Footer>
