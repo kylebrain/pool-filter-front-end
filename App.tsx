50a6f78d8172d4402e88b7b978ed9fd95a1effe7
Kyle Brainard <kbrainard@nevada.unr.edu>
2020-05-21 14:24:40 -0700

    Add tabs and edit the header/footer

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import React, { Component, useState, useEffect } from 'react'
-import {
-    StyleSheet,
-    TouchableOpacity,
-    Text,
-    View,
-    FlatList,
-} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
+import { View, Linking } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, List, ListItem, Grid, Row, Tab, Tabs } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 interface PoolTimer
 {
-    "id": number,
-    "speed": number,
-    "start": string,
-    "summer_duration": string,
-    "winter_duration": string
+    id: number,
+    speed: number,
+    start: string,
+    summer_duration: string,
+    winter_duration: string
 }
+
+const SERVER_NAME = 'http://192.168.4.32:5000';
 
 const TimerList : React.FC = () => {
     const [error, setError] = useState<{message:string} | null>(null);
@@ -25,7 +26,7 @@ const TimerList : React.FC = () => {
     // this useEffect will run once
     // similar to componentDidMount()
     useEffect(() => {
-        fetch("http://localhost:5000/program/all")
+        fetch(SERVER_NAME + "/program/all")
             .then(res => res.json())
             .then(
                 (result) => {
@@ -46,26 +47,70 @@ const TimerList : React.FC = () => {
     if (error) {
         return <Text>Error: {error.message}</Text>;
     } else if (!isLoaded) {
-        return <Text>Loading...</Text>;
+        return <AppLoading/>
     } else {
         return (
-            <FlatList
-                data={items}
-                renderItem={({item}) => <Text>{item.speed} - {item.start}</Text>}>
-            </FlatList>
+            <List>
+                {items.map(item => <ListItem key={item.id.toString()}><Text>{item.speed} - {item.start}</Text></ListItem>)}
+            </List>
         );
     }
 }
 
 const App : React.FC = () => {
-    return <View style={[styles.center, {top: 50}]}><TimerList/></View>
-}
 
-const styles = StyleSheet.create({
-    center: {
-      alignItems: 'center'
+    let [fontsLoaded] = useFonts({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        ...Ionicons.font,
+        });    
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return(
+            <Container>
+                <Header hasTabs>
+                <Left>
+                    <Button transparent>
+                        <Icon name='cup' type='MaterialCommunityIcons' style={{color: 'white'}}/>
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Pool Slick</Title>
+                </Body>
+                <Right />
+                </Header>
+                <Tabs>
+                    <Tab heading="Dashboard">
+                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF'}}>
+                            <View style={{flex: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center', backgroundColor:'#F5FCFF'}}>
+                                <Text>Section 1</Text>
+                                <TimerList/>
+                            </View>
+                            <View style={{flex:1, alignSelf: 'stretch', backgroundColor:'#F5FCFF'}}>
+                                <Text>Section 2</Text>
+                                <TimerList/>
+                            </View>
+                        </View>
+                    </Tab>
+                    <Tab heading="Timers">
+                        <Text>Welcome to Timers!</Text>
+                    </Tab>
+                    <Tab heading="Settings">
+                    <Text>Welcome to Settings!</Text>
+                    </Tab>
+                </Tabs>
+                <Footer>
+                <FooterTab>
+                    <Button full onPress={ ()=>{ Linking.openURL('https://github.com/kylebrain')}}>
+                    <Text>By Kyle Brainard</Text>
+                    </Button>
+                </FooterTab>
+                </Footer>
+            </Container>
+        );
     }
-  })
-  
+} 
 
 export default App;
